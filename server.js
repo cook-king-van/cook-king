57a5678f23f7c77067cfc7452ce88932fde756af
Jay Han <jay.han01@sap.com>
2022-09-18 23:39:15 -0700

    proxy

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
