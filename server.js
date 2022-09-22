be24f55fc11c20601619269daf29b4bdd3655c92
Jay Han <jay.han01@sap.com>
2022-09-21 21:38:55 -0700

    Setup mongodb

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db';
+
+dotenv.config();
+
+// Connect Database
+connectDB();
 
 const app = express();
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('hello');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8888;
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
