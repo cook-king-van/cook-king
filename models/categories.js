fe4b033f13d0bb7486173abe626613b852e94a6f
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-09-28 06:37:36 +0900

    story: Format Prettier

--- models/categories.js
+++ models/categories.js
@@ -1,12 +1,20 @@
 import mongoose, { Schema } from 'mongoose';
 
 var Categories = new Schema({
-  category_name: { type: String },
-  food_list: { type: [Shema.type.ObjectId], ref: 'Food' },
+  category_name: {
+    type: String
+  },
+  food_list: {
+    type: [Shema.type.ObjectId],
+    ref: 'Food'
+  },
 });
 
 var Option = new Schema({
-  food_id: { type: Shema.type.ObjectId, ref: 'Food' },
+  food_id: {
+    type: Shema.type.ObjectId,
+    ref: 'Food'
+  },
   type: String,
   enum: {
     value: ['best', 'brunch', 'snack', 'none'],
--- models/food.js
+++ models/food.js
@@ -1,27 +1,77 @@
 import mongoose, { Schema } from 'mongoose';
 var Cooking = new Schema({
-  order: { type: Number, default: 0 },
-  description: { type: String, default: '', },
-  image: { type: Buffer, },
-  create_at: { type: Date, required: true, default: () => Date.now(), },
-  update_at: { type: Date, requried: true, default: () => Date.now(), },
+  order: {
+    type: Number,
+    default: 0
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+  image: {
+    type: Buffer,
+  },
+  create_at: {
+    type: Date,
+    required: true,
+    default: () => Date.now(),
+  },
+  update_at: {
+    type: Date,
+    requried: true,
+    default: () => Date.now(),
+  },
 });
 var Food = new Schema({
-  user_id: { type: Schema.type.ObjectId, ref: 'User' },
-  categories_id: { type: [Schema.type.ObjectId], ref: 'Categories' }, //N:M relation
-  food_name: { type: String, requried: true },
-  tag: { type: [String], unique: true, default: undefined },
-  size: { type: Number, requried: ture },
-  time: { type: Number, requried: ture },
-  ingredient: { type: [String], unique: true, default: undefined },
+  user_id: {
+    type: Schema.type.ObjectId,
+    ref: 'User'
+  },
+  categories_id: {
+    type: [Schema.type.ObjectId],
+    ref: 'Categories'
+  }, //N:M relation
+  food_name: {
+    type: String,
+    requried: true
+  },
+  tag: {
+    type: [String], unique: true,
+    default: undefined
+  },
+  size: {
+    type: Number,
+    requried: ture
+  },
+  time: {
+    type: Number,
+    requried: ture
+  },
+  ingredient: {
+    type: [String],
+    unique: true, default: undefined
+  },
   food_media: { type: [String] },
-  option: { type: String, ref: 'Option', default: 'none' },
-  image: { type: Buffer },
+  option: {
+    type: String,
+    ref: 'Option', default: 'none'
+  },
+  image: {
+    type: Buffer
+  },
   steps: {
     type: [Cooking],
     default: undefined,
   },
-  create_at: { type: Date, required: true, default: () => Date.now() },
-  update_at: { type: Date, requried: true, default: () => Date.now() },
+  create_at: {
+    type: Date,
+    required: true,
+    default: () => Date.now()
+  },
+  update_at: {
+    type: Date,
+    requried: true,
+    default: () => Date.now()
+  },
 });
 module.exports = mongoose.model('Food', Food);
