5153d2d6e4522cea0434081320b0521de49d73c2
Jieun Yu <55225394+jieunyu0623@users.noreply.github.com>
2022-09-27 00:51:59 -0700

    task: P1-39 add initial redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/register' element={<RegisterPage />} />
-      <Route path='/' element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/" element={<LandingPage />} />
     </Routes>
   );
 };
---
+++ client/src/actions/userActions.js
@@ -0,0 +1,1 @@
+export const login = (email, password) => async (dispatch) => {};
