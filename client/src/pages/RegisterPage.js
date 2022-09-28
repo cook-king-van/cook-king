c624014a4c5afad5cc978468c17198892c2feadf
Jieun Yu <55225394+jieunyu0623@users.noreply.github.com>
2022-09-28 13:33:17 -0700

    task: creating RegisterPage

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+
+import styles from "./Login.module.css";
+import logo from "../images/logo.png";
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <div className={styles.screen}>
+      <div className={styles.container}>
+        <img src={logo} alt="Logo" className={styles.logo} />
+        <p className={styles.register}>REGISTER</p>
+        <form>
+          <label className={styles.email} htmlFor="email">
+            EMAIL
+          </label>
+          <input type="email" name="email" className={styles.authInputs} />
+          <label className={styles.password} htmlFor="password">
+            PASSWORD
+          </label>
+          <input
+            type="password"
+            name="password"
+            className={styles.authInputs}
+          />
+          <label className={styles.password} htmlFor="password">
+            CONFIRM PASSWORD
+          </label>
+          <input
+            type="password"
+            name="passwordcheck"
+            className={styles.authInputs}
+          />
+          <button type="submit" className={styles.authButton}>
+            REGISTER
+          </button>
+        </form>
+        <a className={styles.accountMsg} href="/login">
+          Already have an account ? <u>Sign In</u>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
