6076dfcf7aceb5f85746f70326af73b861016a18
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-09-30 23:07:12 -0700

    landing page for code review

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import logo from "./pictures/Logo.png";
 import search from "./pictures/Google Web Search.png"
+import pencil from "./pictures/Pencil.png"
+import maleUser from "./pictures/Male User.png"
 import styles from "./LandingPage.module.css";
 
 
 const LandingPage = () => {
   return (
     <section className={styles.header_container}>
-      <img src={logo} alt="logo" className={styles.logo}/>
+      <img src={logo} alt="logo"/>
+
       <button className={styles.primarybth}>Category</button>
 
       <form action="submit" className={styles.search_bar}>
         <input type="text" placeholder='Search bar' className={styles.bar} />
-        <img src={search} alt="search" />
+        <img src={search} alt="search" className={styles.search_image} />
       </form>
+
+    <div className={styles.interface}>
+      <img src={pencil} alt="" className={styles.pencil} />
+      <img src={maleUser} alt="" className={styles.user}/>
+    </div>
     </section>
 
   )
