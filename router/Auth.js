039b5b6cd1ceaf915df605c0d98b32419950bbad
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-10-11 14:51:27 +0900

    fix: Router active

--- router/Auth.js
+++ router/Auth.js
@@ -1,11 +1,11 @@
 import express,{Router} from 'express';
-// import GetUser from "../api/auth/GetUser";
+import Auth from "../api/auth/Auth";
 import Login from "../api/auth/Login";
 import Register from "../api/auth/Register";
 
 const router = express.Router();
 router.post('/login', Login);
 router.post('/register', Register);
-// router.get('/user', GetUser);
+router.get('/user', Auth);
 
 export default router
