e7993bc5a7692c78ab86ceeb0fa95420dcc4c9b8
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-10-16 13:13:37 -0700

    task: half completed, but arrow styles problem with importing css

--- client/src/pages/MainList.js
+++ client/src/pages/MainList.js
@@ -1,72 +1,128 @@
 import React from 'react';
-import styles from "./MainList.modeule.css";
-import tempFood from "../images/tempFood.png";
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import css from './MainList.module.css';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { Image } from "semantic-ui-react";
 
 const MainList = () => {
   
+  //mock data
   const slideImages = [
     {
-      url: tempFood,
+      url: 'https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
       caption: 'Slide 1',
       heart: 135,
-      user: "Jay"
+      user: 'Jay',
     },
     {
-      url: tempFood,
+      url: 'https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
       caption: 'Slide 2',
       heart: 12,
-      user: "Yun"
+      user: 'Yun',
     },
     {
-      url: tempFood,
+      url: 'https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
       caption: 'Slide 3',
       heart: 138,
-      user: "Hoon"
-    }
+      user: 'Hoon',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+      caption: 'Slide 4',
+      heart: 138,
+      user: 'Hoon',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      caption: 'Slide 5',
+      heart: 138,
+      user: 'Hoon',
+    },
   ];
+  
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
 
+  //entire slideContainer
   const SlideCreator = (props) => {
     
-    return (
-      <div className='slide'>
-      <div>
-        <img src={tempFood} alt='' style={{width: "100%"}} />
-      </div>
-      <section className='slideDescription'>
-        <p>{props.title}</p>
-        <div className='slideRating' style={{display: "flex", justifyContent: "space-between"}} id="12">
-          <p>❤{props.heart}</p>
-          <p>{props.user}</p>  
+    //each of the slide card
+    const SlideCard = (props) => {
+      return (
+        <div className={css.image_item}>
+         <Image
+            draggable={false}
+            style={{ width: "100%", height: "80%" }}
+            src={props.image.url}
+            key={props.image.caption}
+          />
+          <h3 className={css.h3}>{props.description}</h3>
+          <div className={css.cardInfo}>
+            <p>{props.heart}</p>
+            <p>{props.user}</p>
+          </div>
         </div>
-      </section>
-    </div>
-    )
-  }
+
+      )
+    }
+
+
+    return (
+   <Carousel
+      ssr
+      partialVisbile
+      itemClass="image-item"
+      responsive={responsive}
+      arrows={true}
+      infinite
+      className={css.reactMultiCarouselList}
+    >
+      {slideImages.slice(0, slideImages.length).map(image => {
+        return (
+         <SlideCard
+         image={image}
+         description={image.caption}
+         heart={image.heart}
+         user={image.user}
+         />
+        );
+      })}
+    </Carousel>
+    );
+  };
+
 
 
   return (
     <>
       <div>
-        <h1 className={styles.h1}>Today's Best</h1>
+        <h1 className={css.h1}>Today's Best</h1>
       </div>
 
-    <div className='slideShow'>
-      <div className="slideContainer">
-        {slideImages.map((image, index) => {
-          return(
-            <SlideCreator title={image.caption} heart={image.heart} user={image.user} id={index} />
-          )
-        })}
+      <div className={css.slideShow}>
+        <div className={css.slideContainer}>
+          <SlideCreator />
+        </div>
       </div>
-      <div id="Arrows" onClick={() => console.log("clicked")}>          
-        <ChevronLeftIcon />
-        <ChevronRightIcon />
-      </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
 export default MainList;
