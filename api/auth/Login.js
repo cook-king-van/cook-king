668c4c475204648c5f48893b950a034478dfb935
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-10-17 12:02:49 +0900

    fix: Fix properteis

--- api/auth/Login.js
+++ api/auth/Login.js
@@ -37,7 +37,7 @@ const Login = async (req, res) => {
     return res.status(200).json({
       status: 200,
       token: access,
-      user:user,
+      user,
       message: `${user.name} signed in successfully`,
     });
   } catch (e) {
