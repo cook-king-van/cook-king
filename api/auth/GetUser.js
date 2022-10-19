9d9f46dfd1349a30fadc0e130dfce02463730206
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-10-20 07:19:38 +0900

    fix: Polish GetUser API

--- api/auth/GetUser.js
+++ api/auth/GetUser.js
@@ -1,23 +1,8 @@
-import { Token } from '../../config/redis';
 const GetUser = async (req, res) => {
   try {
-    let token = req.headers.authorization.split(' ')[1];
-    if (!token) {
-      return res.status(401).json({
-        status: 401,
-        message: `There is no Token`,
-      });
-    }
-    const user = Token().AccessVerify(token);
-    if (!user) {
-      return res.status(402).json({
-        status: 402,
-        message: `Token expired`,
-      });
-    }
     return res.status(200).json({
       status: 200,
-      user: user.payload,
+      user: req.user,
     });
   } catch (e) {
     console.error(`Exception Error`);
