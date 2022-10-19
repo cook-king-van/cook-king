f6ada06582e159a19ff3c5d285995ad632b38698
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-10-20 07:17:10 +0900

    fix: Fix Router

--- router/index.js
+++ router/index.js
@@ -1,8 +1,11 @@
-import express,{Router} from 'express'
+import express from 'express';
 import AuthRouter from './Auth';
-
+import UserRouter from './User';
+import FoodFouter from './Food';
 const router = express.Router();
 
 router.use('/auth', AuthRouter);
+router.use('/users', UserRouter);
+router.use('/foods', FoodFouter);
 
 export default router;
