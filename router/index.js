bde6926a9fc8ad691fbf53661b07c33dd85d0095
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-10-21 06:28:20 +0900

    fix: Food to Recipe

--- router/index.js
+++ router/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import AuthRouter from './Auth';
 import UserRouter from './User';
-import FoodFouter from './Food';
+import RecipeRouter from './Recipe';
 const router = express.Router();
 
 router.use('/auth', AuthRouter);
 router.use('/users', UserRouter);
-router.use('/foods', FoodFouter);
+router.use('/recipes', RecipeRouter);
 
 export default router;
