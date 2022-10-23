ac2269221e4ca0a90ea2469c5c28d75a05fc1063
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-10-23 16:43:07 +0900

    story: P1-45 Get All Recipe API

--- router/Recipe.js
+++ router/Recipe.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import CreateRecipe from '../api/foods/PostRecipe';
-import GetAllRecipe from '../api/foods/GetAllRecipe';
+import CreateRecipe from '../api/recipes/PostRecipe';
+import GetAllRecipe from '../api/recipes/GetAllRecipe';
 import UserValid from '../api/auth/UserValid';
 
 const router = express.Router();
