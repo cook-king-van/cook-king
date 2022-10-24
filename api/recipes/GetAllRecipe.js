a3983e49eafd9558b485102f0d1cd8ce22445fc4
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-10-24 13:37:00 +0900

    fix: Prettier

--- api/recipes/GetAllRecipe.js
+++ api/recipes/GetAllRecipe.js
@@ -1,14 +1,13 @@
-import Recipe from "../../models/recipe"
-const GetAllRecipe = async(req,res) => {
-    try {
-        const recipes = await Recipe.find({});
-        return res.status(200).send(recipes);
-    }
-    catch (e) {
-        console.error(`Exception Error`);
-        return res.status(500).send(e.message);
-    }
-}
+import Recipe from '../../models/recipe';
+const GetAllRecipe = async (req, res) => {
+  try {
+    const recipes = await Recipe.find({});
+    return res.status(200).send(recipes);
+  } catch (e) {
+    console.error(`Exception Error`);
+    return res.status(500).send(e.message);
+  }
+};
 
 export default GetAllRecipe;
 
