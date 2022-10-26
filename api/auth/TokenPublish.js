5e15306c82d1fc4e22131c6896bec01a99ef072e
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-10-27 06:08:54 +0900

    task: P1-79 Token Publish

---
+++ api/auth/TokenPublish.js
@@ -0,0 +1,41 @@
+import { Token, getValue, Rename, Logout } from '../../config/redis';
+const TokenPublish = async (req, res) => {
+  try {
+    let token = req.headers.authorization.split(' ')[1]; //header token
+    if (!token) {
+      return res.status(401).send('No Token');
+    }
+    const refresh = await getValue(token);
+    if (!refresh) {
+      return res.status(402).send('Need to login');
+    }
+    const data = await Token().RefreshVerify(refresh);
+    if (!data) {
+      await Logout(token);
+      return res.status(403).send('Refresh Token expired');
+    }
+    const { _id, name } = data.payload;
+    const access = await Token().Access({ _id, name }); //encrypt
+    await Rename(token, access); //Change name
+    return res.status(200).json({
+      token: access,
+      message: `Refresh Token published`,
+    });
+  } catch (e) {
+    console.error(`Exception Error`);
+    return res.status(500).send(e.message);
+  }
+};
+export default TokenPublish;
+
+/**
+ *  Error Code
+ *
+ *  401 No Token
+ *  402 Need to login
+ *  403 Refresh Token expired
+ *  500 Exception Error
+ *
+ *  Response Code
+ *  200
+ */
