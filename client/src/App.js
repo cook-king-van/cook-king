d3a2eac180ec4ef1657f4c38f27c96c97077f657
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-10-30 18:15:42 -0700

    story:P1-76 complete only resut cards and container

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,12 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginRecoveryPage from './pages/LoginRecoveryPage';
 import UserProfilePage from './pages/UserProfile';
+import { SearchPage } from './pages/SearchPage';
 
 const App = () => {
   return (
     <Routes>
+      <Route path='/' element={<SearchPage />} exact />
       <Route path='/login' element={<LoginPage />} />
       <Route path='/register' element={<RegisterPage />} />
       <Route path='/login-recovery' element={<LoginRecoveryPage />} />
---
+++ client/src/pages/SearchPage.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import { NavBar } from '../components/navbar/NavBar'
+import "./SearchPage.css"
+import heart from "../images/Heart.png";
+import tempFood from "../images/tempFood.png"
+import tempFood2 from "../images/TempFood2.png"
+import tempFood3 from "../images/TempFood3.png"
+import { ImageList, ImageListItem, ImageListItemBar } from '@mui/material';
+
+export const SearchPage = (props) => {
+  //mock data
+  let res = 
+    [
+      {
+        url: tempFood,
+        caption: 'Slide 1',
+        heart: 135,
+        user: 'Jay',
+      },
+      {
+        url: tempFood2,
+        caption: 'Slide 2',
+        heart: 12,
+        user: 'Yun',
+      },
+      {
+        url: tempFood3,
+        caption: 'Slide 3',
+        heart: 138,
+        user: 'Hoon',
+      },
+      {
+        url: tempFood,
+        caption: 'Slide 4',
+        heart: 138,
+        user: 'Hoon',
+      },
+      {
+        url: tempFood2,
+        caption: 'Slide 5',
+        heart: 138,
+        user: 'Hoon',
+      },
+      {
+        url: tempFood2,
+        caption: 'Slide 5',
+        heart: 138,
+        user: 'Hoon',
+      },
+      {
+        url: tempFood2,
+        caption: 'Slide 5',
+        heart: 138,
+        user: 'Hoon',
+      }
+    ]
+  
+  let Card = (item) => {
+    let eachItem = item.item;
+    return (
+      <>
+        <h5>{eachItem.caption}</h5>
+        <div className='search-title'>
+          <p style={{alignItems: "center", display: 'flex'}}><img src={heart} alt=""></img>{eachItem.heart}</p>
+          <p>{eachItem.user}</p>
+        </div>
+      </>
+    )
+  }
+
+
+  return (
+    <div>
+      <NavBar />
+      <button>Sort</button>
+      <div className='search-container'>
+        <ImageList sx={{width: '100%'}}  cols={3} rowHeight={250}>
+          
+          {res.map((e, index) => (
+             <ImageListItem key={index} className='search-card'>
+             <img
+              // style={{ width: "250px", height: "255px" }}
+               src={`${e.url}?w=164&h=164&fit=crop&auto=format`}
+               srcSet={`${e.url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+               alt={e.caption}
+               loading="lazy"
+             />
+              <ImageListItemBar position="below" title={<Card item={e} />} />
+           </ImageListItem>
+           
+          ))}
+        </ImageList>
+      </div>
+    </div>
+  )
+}
