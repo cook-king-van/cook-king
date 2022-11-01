235a30a7b617511a8480c7b296d307ebaca98943
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-11-01 15:10:02 -0700

    story: P1-61 Like Recipe

---
+++ api/recipes/LikeRecipe.js
@@ -0,0 +1,44 @@
+import Recipe from '../../models/recipe';
+const LikeRecipe = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const userName = req.user.name;
+    const Id = req.params.recipeId;
+    const ret = await Recipe.findOneAndUpdate(
+      {
+        _id: Id,
+        userLike: {
+          $nin: userId,
+        },
+      },
+      {
+        $push: {
+          userLike: userId,
+        },
+        $inc: {
+          likeCount: 1,
+        },
+      }
+    );
+    if (!ret) {
+      return res.status(403).send(`${userName} already liked ${Id} recipe`);
+    }
+    return res.status(200).send(`${userName} like ${Id} recipe`);
+  } catch (e) {
+    console.error(`Exception Error`);
+    return res.status(500).send(e.message);
+  }
+};
+export default LikeRecipe;
+
+/**
+ *  Error Code
+ *
+ *  401 No Token
+ *  402 Token expried
+ *  403 Already Liked
+ *  500 Exception Error
+ *
+ *  Response Code
+ *  200
+ */
