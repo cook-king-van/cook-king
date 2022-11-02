3c3fb9bb10a7a560d91df5a3cc5333fb452c06f2
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-11-02 13:49:11 -0700

    story: P1-48 Update user profile

--- router/User.js
+++ router/User.js
@@ -2,9 +2,10 @@ import express from 'express';
 import UserValid from '../api/auth/UserValid';
 import UserRecipe from '../api/users/UserRecipe';
 import UserLikeRecipe from '../api/users/UserLikeRecipe';
+import EditUserinfo from '../api/users/EditUserinfo';
 const router = express.Router();
 
 router.get('/:userId/recipes', UserValid, UserRecipe);
 router.get('/:userId/likes', UserValid, UserLikeRecipe);
-
+router.put('/',UserValid,EditUserinfo);
 export default router;
