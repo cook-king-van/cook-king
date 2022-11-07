b958b84055e6392eb7d70f76e0e018031e3bda3f
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-11-06 17:28:29 -0800

    fix: quote clean up

--- client/src/App.js
+++ client/src/App.js
@@ -11,12 +11,12 @@ import { SearchPage } from './pages/SearchPage';
 const App = () => {
   return (
     <Routes>
-        <Route path='/' element={<LandingPage />} exact />
-      <Route path='/search' element={<SearchPage />} exact />
       <Route path='/login' element={<LoginPage />} />
       <Route path='/register' element={<RegisterPage />} />
       <Route path='/login-recovery' element={<LoginRecoveryPage />} />
       <Route element={<PrivateRoute />}>
+      <Route path='/' element={<LandingPage />} exact />
+      <Route path='/search' element={<SearchPage />} exact />
         <Route path='/profile' element={<UserProfilePage />} exact />
       </Route>
     </Routes>
