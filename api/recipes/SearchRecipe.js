db14d0209869a2fbf9b2f3be112387fba5649f8f
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-11-18 15:15:22 -0800

    story:P1-88 Get Recipe Search API

--- api/recipes/SearchRecipe.js
+++ api/recipes/SearchRecipe.js
@@ -1,14 +1,29 @@
+import categories from '../../models/categories';
 import Recipe from '../../models/recipe';
+// import Tag from '../../models/tag';
 const SearchRecipe = async (req, res) => {
   try {
-    const query = req.query;
-    const IncludeWords = new RegExp(query.name, 'i'); //Parse Include words
-    const recipes = await Recipe.find({
+    const name = req.query.name;
+    const FindRecipes = await Recipe.find({
+      // Using regex include name
       recipeName: {
-        $regex: IncludeWords,
+        $regex: name,
+        $options: 'i',
       },
-    });
-    res.status(200).send(recipes);
+    }).select('recipeName recipeImage likeCount');
+    const TagRecipes = await categories.Tag.find({
+      tagName: {
+        $regex: name,
+        $options: 'i',
+      },
+    })
+      .select('-_id -tagName -__v')
+      .populate({
+        path: 'recipeId',
+        select: ['recipeName', 'likeCount'],
+      });
+    const recipes = FindRecipes.concat(TagRecipes[0].recipeId); // connect two arrays
+    res.status(200).send({ recipes });
   } catch (e) {
     console.error(`Exception Error`);
     return res.status(500).send(e.message);
