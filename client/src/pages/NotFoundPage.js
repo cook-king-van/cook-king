b5ef856d84344073380ff5c3873d381443cba0ff
Jieun Yu <55225394+jieunyu0623@users.noreply.github.com>
2022-12-05 17:44:32 -0800

    story: NotFoundPage UI done

--- client/src/pages/NotFoundPage.js
+++ client/src/pages/NotFoundPage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './NotFoundPage.css';
+import notFound from '../images/error404.png';
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className='NotFoundPage-container'>
+      <img src={notFound} alt='404' />
+      Page Not Found
+    </div>
+  );
 };
 
 export default NotFoundPage;
