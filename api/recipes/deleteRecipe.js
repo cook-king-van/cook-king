8635837e52644cbd296c5898bb3de6c096a8c73a
younghoonyou <kcnoh2@hanyang.ac.kr>
2022-12-07 23:36:44 -0800

    Story: P1-46 Delete Recipe

---
+++ api/recipes/deleteRecipe.js
@@ -0,0 +1,16 @@
+import Recipe from '../../models/recipe';
+const deleteRecipe = async (req, res) => {
+  try {
+    const recipeId = req.params.recipeId;
+    const recipe = await Recipe.findByIdAndDelete(recipeId);
+    if (!recipe) {
+      return res.status(403).send(`There is no recipe`);
+    }
+    return res.send(`Recipe deleted`);
+  } catch (e) {
+    console.error(`Exception Error`);
+    return res.status(500).send(e.message);
+  }
+};
+
+export default deleteRecipe;
