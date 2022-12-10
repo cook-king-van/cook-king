a38112aaafb21529a0e38709668b18743c24b0fe
Jieun Yu <55225394+jieunyu0623@users.noreply.github.com>
2022-12-09 20:24:19 -0800

    task: pr feedback

--- client/src/utils/SetAuthToken.js
+++ client/src/utils/SetAuthToken.js
@@ -3,20 +3,13 @@ import api from './api';
 // store our JWT in LS/SS and set axios headers if we do have a token
 
 const setAuthToken = (token, isLocal) => {
+  const storage = isLocal ? localStorage : sessionStorage;
   if (token) {
     api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    if (isLocal) {
-      localStorage.setItem('token', token);
-    } else {
-      sessionStorage.setItem('token', token);
-    }
+    storage.setItem('token', token);
   } else {
     delete api.defaults.headers.common['Authorization'];
-    if (isLocal) {
-      localStorage.removeItem('token');
-    } else {
-      sessionStorage.removeItem('token');
-    }
+    storage.removeItem('token');
   }
 };
 
