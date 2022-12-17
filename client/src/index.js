33aee21f9df4b30e5696a4dd2a4f62698400252c
Jieun Yu <55225394+jieunyu0623@users.noreply.github.com>
2022-12-17 15:32:47 -0800

    story: prevents child component to be affected by parent component (scroll to middle of page then going to child component makes the screen stay in the middle of the page.

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,16 @@ import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './lib/ScrollToTop';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
     </BrowserRouter>
   </Provider>
 );
