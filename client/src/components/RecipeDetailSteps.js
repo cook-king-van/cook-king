9e029d540bd37daccba1160644c73f570f5f40de
Jieun Yu <55225394+jieunyu0623@users.noreply.github.com>
2022-12-18 14:00:03 -0800

    story: componentization for RecipeDetailPage

---
+++ client/src/components/RecipeDetailSteps.js
@@ -0,0 +1,95 @@
+import React from 'react';
+
+const RecipeDetailSteps = (props) => {
+  const {
+    isTabView1On,
+    tabView1On,
+    tabView1,
+    tabView1OnClick,
+    tabView2OnClick,
+    isTabView2On,
+    tabView2On,
+    tabView2,
+    tabView3OnClick,
+    isTabView3On,
+    tabView3On,
+    tabView3,
+    steps,
+  } = props;
+  return (
+    <div className='RecipeDetail-stepContainer'>
+      <div className='RecipeDetail-stepTitleContainer'>
+        <label className='RecipeDetail-stepTitle'>Steps</label>
+        <div>
+          <button className='RecipeDetail-stepBtnBox' onClick={tabView1OnClick}>
+            <img
+              src={isTabView1On ? tabView1On : tabView1}
+              alt='imgTextView'
+              className='RecipeDetail-imageTextIcon'
+            />
+          </button>
+          <button className='RecipeDetail-stepBtnBox' onClick={tabView2OnClick}>
+            <img
+              src={isTabView2On ? tabView2On : tabView2}
+              alt='imgTextView'
+              className='RecipeDetail-imageTextIcon'
+            />
+          </button>
+          <button className='RecipeDetail-stepBtnBox' onClick={tabView3OnClick}>
+            <img
+              src={isTabView3On ? tabView3On : tabView3}
+              alt='imgTextView'
+              className='RecipeDetail-imageTextIcon'
+            />
+          </button>
+        </div>
+      </div>
+      <div
+        className={
+          isTabView1On
+            ? 'RecipeDetail-tabView1'
+            : isTabView2On
+            ? 'RecipeDetail-tabView2'
+            : 'RecipeDetail-tabView3'
+        }>
+        {steps?.length > 1 ? (
+          steps?.map((step, i) => (
+            <div key={step._id} className='RecipeDetail-stepBox'>
+              {isTabView1On || isTabView2On ? (
+                <>
+                  <div className='RecipeDetail-stepTextBox'>
+                    <div className='RecipeDetail-stepNumber'>{i + 1}</div>
+                    <p className='RecipeDetail-stepText'>{step.description}</p>
+                  </div>
+                  {isTabView1On && (
+                    <img
+                      src={step.stepImage}
+                      alt='no img available'
+                      className='RecipeDetail-stepPhoto'
+                    />
+                  )}
+                </>
+              ) : (
+                <div className='RecipeDetail-tabView3Box'>
+                  <div className='RecipeDetail-tabView3TextBox'>
+                    <div className='RecipeDetail-stepNumber'>{i + 1}</div>
+                    <p className='RecipeDetail-stepText'>{step.description}</p>
+                  </div>
+                  <img
+                    src={step.stepImage}
+                    alt='no img available'
+                    className='RecipeDetail-tabView3stepPhoto'
+                  />
+                </div>
+              )}
+            </div>
+          ))
+        ) : (
+          <div>No Steps Found</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default RecipeDetailSteps;
--- client/src/pages/RecipeDetailPage.js
+++ client/src/pages/RecipeDetailPage.js
@@ -14,11 +14,10 @@ import tabView2 from '../images/tab_view2.png';
 import tabView2On from '../images/tab_view2_on.png';
 import tabView3 from '../images/tab_view3.png';
 import tabView3On from '../images/tab_view3_on.png';
-import { Avatar } from '@mui/material';
-import MyCookingCard from '../components/MyCookingCard';
-import Carousel from 'react-multi-carousel';
-import { responsive } from '../utils/carousel';
 import RecipeDetailHeader from '../components/RecipeDetailHeader';
+import RecipeDetailIngredients from '../components/RecipeDetail/RecipeDetailIngredients';
+import RecipeDetailSteps from '../components/RecipeDetailSteps';
+import RecipeDetailAuthor from '../components/RecipeDetailAuthor';
 
 const RecipeDetailPage = () => {
   const { id } = useParams();
@@ -53,18 +52,6 @@ const RecipeDetailPage = () => {
   const [isLoginMessage, setIsLoginMessage] = useState(false);
   const [isOwner, setIsOwner] = useState(false);
 
-  const CustomRightArrow = ({ onClick }) => {
-    return (
-      <i
-        className='RecipeDetail-carouselRightArrow'
-        onClick={() => onClick()}></i>
-    );
-  };
-
-  const CustomLeftArrow = ({ onClick }) => (
-    <i className='RecipeDetail-carouselLeftArrow' onClick={() => onClick()} />
-  );
-
   const refineRecipeName = (recipeName) => {
     const splitName = recipeName?.split(' ');
     for (let i = 0; i < splitName?.length; i++) {
@@ -76,8 +63,12 @@ const RecipeDetailPage = () => {
   useEffect(() => {
     dispatch(getRecipe(id));
     const liked = recipeLiked?.find((recipe) => recipe?._id === _id);
-    if (liked) setIsLike(true);
-    else setIsLike(false);
+    if (liked) {
+      setIsLike(true);
+    } else {
+      setIsLike(false);
+    }
+
     if (userId?._id === currentUserId) {
       setIsOwner(true);
     } else {
@@ -154,146 +145,30 @@ const RecipeDetailPage = () => {
         size={size}
         time={time}
       />
-      <div className='RecipeDetail-ingredientContainer'>
-        <label className='RecipeDetail-ingredientTitle'>Ingredients</label>
-        {ingredient?.length > 1 ? (
-          ingredient?.map((ing, i) => (
-            <Fragment key={ing._id}>
-              <div className='RecipeDetail-ingredientContent'>
-                <span className='RecipeDetail-ingredients'>
-                  {refineRecipeName(ing.name)}
-                </span>
-                <span className='RecipeDetail-ingredients'>{ing.measure}</span>
-              </div>
-              <p
-                className={
-                  i !== ingredient?.length - 1
-                    ? `RecipeDetail-ingredientBorder`
-                    : ''
-                }></p>
-            </Fragment>
-          ))
-        ) : (
-          <div>No Ingredients Found</div>
-        )}
-      </div>
-      <div className='RecipeDetail-stepContainer'>
-        <div className='RecipeDetail-stepTitleContainer'>
-          <label className='RecipeDetail-stepTitle'>Steps</label>
-          <div>
-            <button className='RecipeDetail-stepBtnBox'>
-              <img
-                src={isTabView1On ? tabView1On : tabView1}
-                alt='imgTextView'
-                className='RecipeDetail-imageTextIcon'
-                onClick={tabView1OnClick}
-              />
-            </button>
-            <button
-              className='RecipeDetail-stepBtnBox'
-              onClick={tabView2OnClick}>
-              <img
-                src={isTabView2On ? tabView2On : tabView2}
-                alt='imgTextView'
-                className='RecipeDetail-imageTextIcon'
-              />
-            </button>
-            <button
-              className='RecipeDetail-stepBtnBox'
-              onClick={tabView3OnClick}>
-              <img
-                src={isTabView3On ? tabView3On : tabView3}
-                alt='imgTextView'
-                className='RecipeDetail-imageTextIcon'
-              />
-            </button>
-          </div>
-        </div>
-        <div
-          className={
-            isTabView1On
-              ? 'RecipeDetail-tabView1'
-              : isTabView2On
-              ? 'RecipeDetail-tabView2'
-              : 'RecipeDetail-tabView3'
-          }>
-          {steps?.length > 1 ? (
-            steps?.map((step, i) => (
-              <div key={step._id} className='RecipeDetail-stepBox'>
-                {isTabView1On || isTabView2On ? (
-                  <>
-                    <div className='RecipeDetail-stepTextBox'>
-                      <div className='RecipeDetail-stepNumber'>{i + 1}</div>
-                      <p className='RecipeDetail-stepText'>
-                        {step.description}
-                      </p>
-                    </div>
-                    {isTabView1On && (
-                      <img
-                        src={step.stepImage}
-                        alt='no img available'
-                        className='RecipeDetail-stepPhoto'
-                      />
-                    )}
-                  </>
-                ) : (
-                  <div className='RecipeDetail-tabView3Box'>
-                    <div className='RecipeDetail-tabView3TextBox'>
-                      <div className='RecipeDetail-stepNumber'>{i + 1}</div>
-                      <p className='RecipeDetail-stepText'>
-                        {step.description}
-                      </p>
-                    </div>
-                    <img
-                      src={step.stepImage}
-                      alt='no img available'
-                      className='RecipeDetail-tabView3stepPhoto'
-                    />
-                  </div>
-                )}
-              </div>
-            ))
-          ) : (
-            <div>No Steps Found</div>
-          )}
-        </div>
-      </div>
-      <div className='RecipeDetail-authorContainer'>
-        <label className='RecipeDetail-authorTitle'>
-          About the author . . .
-        </label>
-        <div className='RecipeDetail-authorProfileHolder'>
-          <Avatar
-            alt={userId?.name}
-            src={userId?.profileImage}
-            sx={{ width: 140, height: 140 }}
-            className='RecipeDetail-authorProfilePhoto'
-          />
-          <div className='RecipeDetail-authorName'>{userId?.name}</div>
-        </div>
-        <Carousel
-          itemClass='MainList-image_item'
-          responsive={responsive}
-          infinite={true}
-          arrows={true}
-          className='MainList-reactMultiCarouselList'
-          customLeftArrow={<CustomLeftArrow />}
-          customRightArrow={<CustomRightArrow />}
-          draggable={true}>
-          {recipes
-            ? recipes?.map((recipe) => {
-                return (
-                  <MyCookingCard
-                    key={recipe._id}
-                    recipe={recipe}
-                    onClick={(e) => recipeOnClick(e, recipe._id)}
-                    className='RecipeDetail-authorRecipesCard'
-                  />
-                );
-              })
-            : []}
-        </Carousel>
-      </div>
+      <RecipeDetailIngredients
+        ingredient={ingredient}
+        refineRecipeName={refineRecipeName}
+      />
+      <RecipeDetailSteps
+        tabView1OnClick={tabView1OnClick}
+        isTabView1On={isTabView1On}
+        tabView1On={tabView1On}
+        tabView1={tabView1}
+        tabView2OnClick={tabView2OnClick}
+        isTabView2On={isTabView2On}
+        tabView2On={tabView2On}
+        tabView2={tabView2}
+        tabView3OnClick={tabView3OnClick}
+        isTabView3On={isTabView3On}
+        tabView3On={tabView3On}
+        tabView3={tabView3}
+        steps={steps}
+      />
+      <RecipeDetailAuthor
+        userId={userId}
+        recipes={recipes}
+        recipeOnClick={recipeOnClick}
+      />
     </>
   );
 };
