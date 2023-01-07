dc20e13b779c9ce23b9e3e936126de0a815834f5
Jieun Yu <55225394+jieunyu0623@users.noreply.github.com>
2023-01-06 16:02:28 -0800

    hotfix: fix server issue

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use('/api', APIrouter);
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
+  server.use('*', express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
